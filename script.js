49c4a319e6aec09db258f7fa4f604e3734acfce3
faheder <160651909+faheder@users.noreply.github.com>
2025-04-28 09:37:33 -0300

    botao

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded'), function(){
+document.addEventListener('DOMContentLoaded', function(){
     const botaoDeAcessibilidade = document.getElementById
-
-    const aumentaFontebotao = document.getElementById('aumentar-fonte');
-    const diminuifonteBotao = document.
-}
+ 
+     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+ 
+     let tamanhoAtualFonte = 1;
+ 
+     aumentaFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte += 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+     })
+ 
+     diminuiFonteBotao.addEventListener('click', function(){
+         tamanhoAtualFonte -= 0.1;
+         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+     })
+ 
+ })
