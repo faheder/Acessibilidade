9a872edcc83cffb733652b006121fbc5a0c21b0d
faheder <160651909+faheder@users.noreply.github.com>
2025-04-28 09:31:45 -0300

    botao

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded'), function(){
+    const botaoDeAcessibilidade = document.getElementById
+
+    const aumentaFontebotao = document.getElementById('aumentar-fonte');
+    const diminuifonteBotao = document.
+}
